886ae935365b5aa09b5cc548daffb68f39e06c88
Adam <adam@vintagetrave;.co.uk>
2026-03-22 23:10:38 +0000

    feat: add TinaCMS config and content JSON files

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'C:/Users/adam_/kirk/tina/__generated__/.cache/1774220979584', url: 'https://content.tinajs.io/2.2/content/ba412ca1-2fd5-4a43-ac55-81244c5d1a25/github/main', token: '15b5989ae1db16ce19d27056234b9dbc4903d514', queries,  });
+export default client;
+  
